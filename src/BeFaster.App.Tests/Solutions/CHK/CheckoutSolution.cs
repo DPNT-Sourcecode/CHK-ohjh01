bb257aff2bd848e7962862c9541801d64196727a
sbx_user1051 <sbx_user1051@169.254.28.21>
2019-09-19 16:19:55 +0000

    Thu Sep 19 16:19:55 UTC 2019

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolution.cs
@@ -7,14 +7,57 @@ namespace BeFaster.App.Tests.Solutions.CHK
     public class CheckoutSolutionTest
     {
         [Test]
-        public void CheckoutInvalid()   
+        public void CheckoutIsInvalid()   
         {
-            Assert.AreEqual(0, CheckoutSolution.Checkout("XYZ"));
+            Assert.AreEqual(-1, CheckoutSolution.Checkout("XYZ"));
         }
 
+        [Test]
+        public void CheckoutIsValid()
+        {
+            Assert.AreNotEqual(-1, CheckoutSolution.Checkout("ABCD"));
+        }
 
+        [Test]
+        public void SimpleBasket()
+        {
+            Assert.AreEqual(115, CheckoutSolution.Checkout("ABCD"));
+        }
 
+        [Test]
+        public void MultipleSkuBasketA()
+        {
+            Assert.AreEqual(100, CheckoutSolution.Checkout("AA"));
+        }
 
-         
+        [Test]
+        public void SpecialOfferBasketA()
+        {
+            Assert.AreEqual(180, CheckoutSolution.Checkout("AAAA"));
+        }
+
+        [Test]
+        public void SpecialOfferBasketB()
+        {
+            Assert.AreEqual(75, CheckoutSolution.Checkout("BB"));
+        }
+
+        [Test]
+        public void MultipleSkuBasketC()
+        {
+            Assert.AreEqual(80, CheckoutSolution.Checkout("CCCC"));
+        }
+
+        [Test]
+        public void MultipleSkuBasketD()
+        {
+            Assert.AreEqual(60, CheckoutSolution.Checkout("DDDD"));
+        }
+
+        [Test]
+        public void UnorderedBasket()
+        {
+            Assert.AreEqual(245, CheckoutSolution.Checkout("DACDBCADA"));
+        }
     }
 }
