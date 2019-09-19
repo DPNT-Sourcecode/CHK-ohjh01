64d03527a01e728bcc43bd65d9a86b17cffa809d
sbx_user1051 <sbx_user1051@169.254.28.21>
2019-09-19 16:10:54 +0000

    Thu Sep 19 16:10:54 UTC 2019

--- src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App.Tests/Solutions/CHK/CheckoutSolution.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using BeFaster.App.Solutions.CHK;
+using NUnit.Framework;
+
+namespace BeFaster.App.Tests.Solutions.CHK
+{
+    [TestFixture]
+    public class CheckoutSolutionTest
+    {
+        [Test]
+        public void CheckoutInvalid()   
+        {
+            Assert.AreEqual(0, CheckoutSolution.Checkout("XYZ"));
+        }
+
+
+
+
+         
+    }
+}
--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -78,5 +78,3 @@ namespace BeFaster.App.Solutions.CHK
         }
     }
 }
-
-
