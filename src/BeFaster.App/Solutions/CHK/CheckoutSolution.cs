97869e059fc87d5f71c72cd5f61b6263e3ef1d2e
sbx_user1051 <sbx_user1051@169.254.28.21>
2019-09-19 16:01:55 +0000

    Thu Sep 19 16:01:55 UTC 2019

--- src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
+++ src/BeFaster.App/Solutions/CHK/CheckoutSolution.cs
@@ -1,4 +1,5 @@
-﻿using BeFaster.Runner.Exceptions;
+﻿using System.Collections.Generic;
+using BeFaster.Runner.Exceptions;
 
 namespace BeFaster.App.Solutions.CHK
 {
@@ -6,7 +7,69 @@ namespace BeFaster.App.Solutions.CHK
     {
         public static int Checkout(string skus)
         {
-            throw new SolutionNotImplementedException();
+            // first group SKUs together by creating and sorting a list
+            skus = skus.ToUpper();
+            var skulist = new List<string>();
+            for (int i =0; i < skus.Length; i++)
+            {
+                skulist.Add(skus.Substring(i, 1));
+            }
+
+            skulist.Sort();
+
+            string last = null;
+            int count = 0;
+            int sum = 0;
+
+            foreach(string s in skulist)
+            {
+                if (s == last)
+                {
+                    count++;
+                }
+                else
+                {
+                    sum += CalculatePrice(last, count);
+                    count = 0;
+                    last = s;
+                }
+            }
+
+
+
+
+
+            return -1;
+        }
+
+        private static int CalculatePrice(string sku, int qty)
+        {
+            if (qty == 0)
+                return 0;
+
+            switch (sku)
+            {
+                case "A":
+                    {
+                        int specials = qty / 3;
+                        int rem = qty - 3 * specials;
+                        return rem * 50 + specials * 130;
+                    }
+                case "B":
+                    {
+                        int specials = qty / 2;
+                        int rem = qty - 2 * specials;
+                        return rem * 30 + specials * 45;
+                    }
+                case "C":
+                    return qty * 20;
+                case "D":
+                    return qty * 15;
+
+                default:
+                    throw new System.Exception("Invalid sku");
+            }
         }
     }
 }
+
